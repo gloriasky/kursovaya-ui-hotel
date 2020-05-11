780dcbfb58ce9e0b1021569d3f455676e8470908
Valadzko Kseniya <volodko.kj@gmail.com>
2020-05-11 11:05:26 +0300

    Save more to local storage

--- src/AuthService.js
+++ src/AuthService.js
@@ -5,15 +5,49 @@ import * as _ from "lodash";
 
 const token_item = 'hub_token';
 const user_item = 'hub_user';
+const room_item = 'room_id';
+const from_item = 'from_id';
+const to_item = 'to_id';
+const booking_item = 'booking_id';
 
 export function login(token, user) {
-    sessionStorage.setItem(token_item, token)
+    sessionStorage.setItem(token_item, token);
     sessionStorage.setItem(user_item, JSON.stringify(user))
+}
+
+export function saveBookingId(bookingId) {
+    sessionStorage.setItem(booking_item, bookingId);
+}
+
+export function getBookingId() {
+    return sessionStorage.getItem(booking_item)
+}
+
+export function saveBookingInfo(roomId, from, to) {
+    sessionStorage.setItem(room_item, roomId);
+    sessionStorage.setItem(from_item, from);
+    sessionStorage.setItem(to_item, to)
+}
+
+export function getRoomId() {
+    return sessionStorage.getItem(room_item)
+}
+
+export function getFrom() {
+    return sessionStorage.getItem(from_item)
+}
+
+export function getTo() {
+    return sessionStorage.getItem(to_item)
 }
 
 export function logout() {
     sessionStorage.removeItem(token_item);
     sessionStorage.removeItem(user_item);
+    sessionStorage.removeItem(room_item);
+    sessionStorage.removeItem(to_item);
+    sessionStorage.removeItem(from_item);
+    sessionStorage.removeItem(booking_item);
 }
 
 export function getToken() {
@@ -48,6 +82,13 @@ export function isTokenExpired(token) {
     catch (err) {
         return false;
     }
+}
+
+export function getDateDifference(dateFrom, dateTo) {
+    let _dateFrom = new Date(dateFrom);
+    let _dateTo = new Date(dateTo);
+    const diffTime = Math.abs(_dateFrom - _dateTo);
+    return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 }
 
 export function  beautifyKey(key){
